12a76c2684b9bfb0a1da2daeee2544404969b752
a.ignatov <>
2022-03-29 16:38:12 +0500

    add SPM

--- Package.swift
+++ Package.swift
@@ -23,12 +23,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "sdk",
-            path: "sdk",
+            path: "sdk/sdk",
             resources: [
-                            .process("./Resources")
+                            .process("../Resources")
                         ]),
         .target(
             name: "networking",
-            path: "networking"),
+            path: "networking/source"),
     ]
 )
