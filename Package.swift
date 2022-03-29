9f6b2be8852f1841a37cc418fd947abfd40cc308
a.ignatov <>
2022-03-29 17:14:51 +0500

    add SPM

--- Package.swift
+++ Package.swift
@@ -9,10 +9,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CloudPaymentsSDK",
-            targets: ["sdk"]),
+            targets: ["CloudpaymentsSDK"]),
         .library(
             name: "CloudPaymentsNetworking",
-            targets: ["networking"]),
+            targets: ["CloudpaymentsNetworking"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,13 +22,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "sdk",
+            name: "CloudpaymentsSDK",
+            dependencies: ["CloudpaymentsNetworking"],
             path: "sdk/sdk",
             resources: [
                             .process("../Resources")
                         ]),
         .target(
-            name: "networking",
+            name: "CloudpaymentsNetworking",
             path: "networking/source"),
     ]
 )
